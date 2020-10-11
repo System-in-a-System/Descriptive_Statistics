81393285503e581df2b3de05734a086d0cea093f
Maryna <textconstruction@protonmail.com>
2020-10-11 18:00:38 +0100

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Descriptive_Statistics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
